5fd191c59671e71b9a3625a12395bb193d81f07b
Amr Ibrahim <38406099+amribrahimali@users.noreply.github.com>
2022-08-05 22:45:00 +0200

    - add simple smooth animation.

--- lib/weather/widgets/next_day_panel.dart
+++ lib/weather/widgets/next_day_panel.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:weather_app/utils/app_colors.dart';
+import 'package:weather_app/utils/side_animation.dart';
 import 'package:weather_repository/weather_repository.dart';
 
 class NextDaysPanel extends StatelessWidget {
@@ -18,62 +19,66 @@ class NextDaysPanel extends StatelessWidget {
         itemCount: weatherData.nextDays?.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              width: screenSize.width / 3,
-              decoration: BoxDecoration(
-                  color: AppColors.accentColor,
-                  borderRadius: BorderRadius.circular(25)),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Align(
-                        alignment: Alignment.topRight,
-                        child: CachedNetworkImage(
-                          imageUrl: weatherData.nextDays![index].iconURL ?? '',
-                          placeholder: (context, url) =>
-                              const CircularProgressIndicator.adaptive(),
-                          errorWidget: (context, url, error) =>
-                              const Icon(Icons.error),
-                        )),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(top: 10, right: 20, left: 20),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        weatherData.nextDays![index].day ?? '',
-                        style: GoogleFonts.rubik(
-                            fontSize: 15, color: Colors.white),
-                      ),
+          return SideInAnimation(
+            index,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: screenSize.width / 3,
+                decoration: BoxDecoration(
+                    color: AppColors.accentColor,
+                    borderRadius: BorderRadius.circular(25)),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Align(
+                          alignment: Alignment.topRight,
+                          child: CachedNetworkImage(
+                            imageUrl:
+                                weatherData.nextDays![index].iconURL ?? '',
+                            placeholder: (context, url) =>
+                                const CircularProgressIndicator.adaptive(),
+                            errorWidget: (context, url, error) =>
+                                const Icon(Icons.error),
+                          )),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 20, left: 20),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: RichText(
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                                text: weatherData.nextDays![index].maxTemp?.c
-                                    .toString(),
-                                style: GoogleFonts.rubik(
-                                    fontSize: 30, color: Colors.white)),
-                            TextSpan(
-                                text: "°C",
-                                style: GoogleFonts.rubik(
-                                    fontSize: 15,
-                                    color: AppColors.selectedColor))
-                          ],
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 10, right: 20, left: 20),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          weatherData.nextDays![index].day ?? '',
+                          style: GoogleFonts.rubik(
+                              fontSize: 15, color: Colors.white),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20, left: 20),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                  text: weatherData.nextDays![index].maxTemp?.c
+                                      .toString(),
+                                  style: GoogleFonts.rubik(
+                                      fontSize: 30, color: Colors.white)),
+                              TextSpan(
+                                  text: "°C",
+                                  style: GoogleFonts.rubik(
+                                      fontSize: 15,
+                                      color: AppColors.selectedColor))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
--- lib/weather/widgets/weather_panel.dart
+++ lib/weather/widgets/weather_panel.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:weather_app/utils/app_colors.dart';
+import 'package:weather_app/utils/fade_animation.dart';
 import 'package:weather_repository/weather_repository.dart';
 
 class WeatherPanel extends StatelessWidget {
@@ -11,80 +12,84 @@ class WeatherPanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var display = MediaQuery.of(context).size;
-    return Padding(
-      padding: const EdgeInsets.all(15),
-      child: Container(
-        decoration: BoxDecoration(
-            color: AppColors.accentColor,
-            borderRadius: BorderRadius.circular(25)),
-        height: display.height / 3,
-        width: display.width,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 25.0, right: 25, left: 25),
-              child: Row(
-                children: [
-                  Text("Today",
-                      style:
-                          GoogleFonts.rubik(color: Colors.white, fontSize: 30)),
-                  const Spacer(),
-                  Text(
-                    weatherData.currentConditions?.dayhour ?? '',
-                    style: GoogleFonts.rubik(fontSize: 15, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10.0, right: 25, left: 25),
-              child: Row(
-                children: [
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                            text: weatherData.currentConditions?.temp?.c
-                                .toString(),
-                            style: GoogleFonts.rubik(
-                                fontSize: 70, color: Colors.white)),
-                        TextSpan(
-                            text: "°C",
-                            style: GoogleFonts.rubik(
-                                fontSize: 70, color: AppColors.selectedColor))
-                      ],
-                    ),
-                  ),
-                  const Spacer(),
-                  CachedNetworkImage(
-                    imageUrl: weatherData.currentConditions?.iconURL ?? '',
-                    placeholder: (context, url) =>
-                        const CircularProgressIndicator.adaptive(),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 15.0, right: 25, left: 25, bottom: 20),
-              child: Row(
-                children: [
-                  const Icon(IconData(0xf193, fontFamily: 'MaterialIcons'),
-                      color: AppColors.selectedColor),
-                  Expanded(
-                    child: Text(
-                      weatherData.region ?? '',
-                      overflow: TextOverflow.ellipsis,
+    return FadeInAnimation(
+      2,
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Container(
+          decoration: BoxDecoration(
+              color: AppColors.accentColor,
+              borderRadius: BorderRadius.circular(25)),
+          height: display.height / 3,
+          width: display.width,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 25.0, right: 25, left: 25),
+                child: Row(
+                  children: [
+                    Text("Today",
+                        style: GoogleFonts.rubik(
+                            color: Colors.white, fontSize: 30)),
+                    const Spacer(),
+                    Text(
+                      weatherData.currentConditions?.dayhour ?? '',
                       style:
                           GoogleFonts.rubik(fontSize: 15, color: Colors.white),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0, right: 25, left: 25),
+                child: Row(
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                              text: weatherData.currentConditions?.temp?.c
+                                  .toString(),
+                              style: GoogleFonts.rubik(
+                                  fontSize: 70, color: Colors.white)),
+                          TextSpan(
+                              text: "°C",
+                              style: GoogleFonts.rubik(
+                                  fontSize: 70, color: AppColors.selectedColor))
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    CachedNetworkImage(
+                      imageUrl: weatherData.currentConditions?.iconURL ?? '',
+                      placeholder: (context, url) =>
+                          const CircularProgressIndicator.adaptive(),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 15.0, right: 25, left: 25, bottom: 20),
+                child: Row(
+                  children: [
+                    const Icon(IconData(0xf193, fontFamily: 'MaterialIcons'),
+                        color: AppColors.selectedColor),
+                    Expanded(
+                      child: Text(
+                        weatherData.region ?? '',
+                        overflow: TextOverflow.ellipsis,
+                        style: GoogleFonts.rubik(
+                            fontSize: 15, color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
